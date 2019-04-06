174a8621c7a67a235f6731d338837292809afc42
abhirup <abhirupraj777@gmail.com>
2019-04-06 21:56:34 +0530

    This is my first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void Browserautomation()
+	{
+		System.out.println("Browserautomation");
+	}
+	@Test
+	public void elementsui()
+	{
+		System.out.println("elementsui");
+	}
+}
